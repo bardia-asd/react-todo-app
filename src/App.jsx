6d50183378a081d53e32fdaea93329b79a063443
bardia-asd <brdyaasdy5@gmail.com>
2025-11-08 20:10:09 +0330

    feat: Add header and TodoForm components with basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import { useState } from "react";
+import TodoForm from "./components/TodoForm";
 
 function App() {
-    return <></>;
+    const [newTaskText, setNewTaskText] = useState("");
+
+    const addTodo = (e) => {
+        e.preventDefault();
+        setNewTaskText("");
+    };
+
+    return (
+        <div className="container max-w-2xl py-8 px-4 mx-auto ">
+            <header className="text-center">
+                <h1 className="font-semibold text-3xl mb-2">My Tasks</h1>
+                <p className="text-slate-500">
+                    Stay organized and get things done
+                </p>
+            </header>
+
+            <main className="mt-10">
+                <TodoForm
+                    value={newTaskText}
+                    onChange={(e) => setNewTaskText(e.target.value)}
+                    onSubmit={addTodo}
+                />
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,26 @@
+import { Plus } from "lucide-react";
+
+export default function TodoForm({ value, onChange, onSubmit }) {
+    return (
+        <form
+            onSubmit={onSubmit}
+            className="bg-white rounded-lg shadow-sm p-4 border border-gray-200">
+            <div className="flex gap-2">
+                <input
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                    placeholder="Add a new task"
+                    className="flex-1 bg-gray-100 px-3 py-1 rounded-lg border border-gray-200 focus:outline-0 focus:ring-4 focus:ring-gray-300 transition-all duration-200"
+                />
+                <button
+                    type="submit"
+                    className="flex items-center gap-2 px-6 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-lg transition-all duration-200 cursor-pointer disabled:opacity-50 disabled:cursor-auto disabled:hover:bg-blue-600 disabled:text-gray-200"
+                    disabled={!value.trim()}>
+                    <Plus size={20} />
+                    Add Task
+                </button>
+            </div>
+        </form>
+    );
+}
